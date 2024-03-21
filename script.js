c19b8f25c0af1a510fdb14f11b9178025bac2d1e
João Pedro Monteiro <154384458+monteiroojp@users.noreply.github.com>
2024-03-21 10:31:07 -0300

    pokedex end

---
+++ script.js
@@ -0,0 +1,85 @@
+//Variáveis
+const searchInput = document.getElementById('searchInpt')
+const nextButton = document.getElementById('nextButton')
+const previousButton = document.getElementById('previousButton')
+const searchButton = document.getElementById('search')
+let pokeImg = document.getElementById('pokeImg')
+let pokemonNumber = document.getElementById('pokemonNumber')
+let pokemonName = document.getElementById('pokemonName')
+let pokemonIndex = 0
+let pokemonData;
+let pokemonDataArray;
+//Funções
+
+async function pokemonShow() {
+    try{
+        //Recebimento da data contendo todos pokemons possíveis da API
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0.")
+        pokemonData = await response.json()
+        //Transformando os possíveis pokemons em uma array
+        pokemonDataArray = [...pokemonData.results]
+        //Igualando o pokemon atual com o índice da variável
+        let currentPokemon = pokemonData.results[pokemonIndex]
+        pokemonName.innerHTML = `- ${currentPokemon.name}`
+        let currentPokemonData = await  (await fetch(currentPokemon.url)).json()
+        console.log(currentPokemon)
+        pokemonNumber.innerHTML = `${currentPokemonData.id}`
+        pokeImg.src = `${currentPokemonData.sprites.front_default}`
+    }
+
+    catch(error){
+        window.alert('Não existe nenhum pokemon condizente a esse número nesse pokedex!')
+    }
+}
+
+pokemonShow()
+
+const nextPokemon = () =>{
+    pokemonIndex++
+    if(pokemonIndex > 1301){
+        pokemonIndex = 0
+        pokemonShow()
+    }
+    else{
+        pokemonShow()
+    }
+}
+
+const previousPokemon = () =>{
+    pokemonIndex--
+    if(pokemonIndex < 0){
+        pokemonIndex = 1301
+        pokemonShow()
+    }
+    else{
+        pokemonShow()
+    }
+}
+
+const searchPokemon = () => {
+    let currentInput = searchInput.value.trim(); 
+    let nameorId = /^[0-9]+$/.test(currentInput);
+    if (nameorId) {
+        pokemonIndex = Number(currentInput) - 1
+        pokemonShow()
+    } 
+    else{
+        let found = false
+        pokemonDataArray.forEach((element, index) =>{
+           if(element.name == currentInput){
+                pokemonIndex = index
+                pokemonShow()
+                found = true
+           }
+        })
+        if(!false){
+            window.alert('Não existe pokemon com esse nome nesse pokedex!')
+        }
+    }
+
+}
+
+//Event Listeners
+nextButton.addEventListener('click',  nextPokemon)
+previousButton.addEventListener('click', previousPokemon)
+searchButton.addEventListener('click', searchPokemon)
